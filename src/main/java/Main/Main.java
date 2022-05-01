17a3eee8bf8a3803ccf77a38d763d8f10d0172c9
LauraAdom <laura.adomaityt@gmail.com>
2022-05-01 10:21:15 +0200

    lookupLeadid method added

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,12 +2,24 @@ package Main;
 
 
 import Classes.CRM;
+import Classes.Lead;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        CRM crm = new CRM();
+        crm.createLead(scanner);
+        crm.createLead(scanner);
+        crm.createLead(scanner);
+        crm.showLeads();
+        crm.lookupLeadId(scanner);
+        crm.lookupLeadId(scanner);
+
+
+
 
 
 
