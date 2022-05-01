7a00189fefeea85b3a03e63e02f0352b9c5f0465
LauraAdom <100992236+LauraAdom@users.noreply.github.com>
2022-05-01 19:29:19 +0200

    createContact method created (#4)

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -10,13 +10,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        CRM crm = new CRM();
-        crm.createLead(scanner);
-        crm.createLead(scanner);
-        crm.createLead(scanner);
-        crm.showLeads();
-        crm.lookupLeadId(scanner);
-        crm.lookupLeadId(scanner);
 
 
 
