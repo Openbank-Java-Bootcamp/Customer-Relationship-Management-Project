2fee4512c4490c16dcd372ffab128db71054d879
Elizabeth Sames <sames@uwalumni.com>
2022-05-03 13:28:50 +0200

    Tests for lookupLead and createContact methods

--- src/main/java/Classes/CRM.java
+++ src/main/java/Classes/CRM.java
@@ -3,7 +3,7 @@ package Classes;
 import java.util.*;
 
 public class CRM {
-    List<Lead> leadList = new ArrayList<>();
+    public Map<String,Lead> leadList = new HashMap();
     private List<Contact> contactList = new ArrayList<>();
 
     public CRM() {
@@ -20,7 +20,8 @@ public class CRM {
         System.out.println("Please type Lead's company's name");
         String leadsCompany = scanner.nextLine();
         Lead newLead = new Lead(leadsName, leadsPhoneNumberAsInt, leadsEmail, leadsCompany);
-        leadList.add(newLead);
+        //leadList.add(newLead);
+        leadList.put(newLead.getId(), newLead);
         System.out.println("Operation successful, Lead is created and added to the Lead's list" + "\nPlease type another command");
     }
 
@@ -30,24 +31,39 @@ public class CRM {
         }
     }
 
-    public void lookupLeadId(Scanner scanner){
-        String inputLine = scanner.nextLine();
-        String[] array = inputLine.split(" ");
-        String id = array[2];
-        for(int i = 0; i < leadList.size(); i++){
-            if(id.equals(leadList.get(i).getId())){
-                System.out.println(leadList.get(i).toString());
-            }
+    public Lead lookupLead(String lead_id) {
+        if (leadList.get(lead_id) == null) {
+            throw new IllegalArgumentException("Not a valid Lead ID");
         }
+        return leadList.get(lead_id);
     }
 
     public Contact createContact(Lead lead){
+        if (lead.getName() == null || lead.getPhoneNumber() == 0 || lead.getEmail() == null) {
+            throw new IllegalArgumentException("Not a valid Lead");
+        }
         String contactName = lead.getName();
         int contactNumber = lead.getPhoneNumber();
         String contactEmail = lead.getEmail();
         Contact newContact = new Contact(contactName, contactNumber, contactEmail);
         contactList.add(newContact);
+        //drop Lead from leadList
+        leadList.remove(lead.getId());
+        //delete Lead
+        deleteLead(lead);
         return newContact;
+    }
+
+    //can't delete a Lead object, but can set all info to null
+    public void deleteLead(Lead lead) {
+        if (lead.getName() == null) {
+            throw new IllegalArgumentException("Not a valid Lead.");
+        }
+        lead.setIdToNull();
+        lead.setName(null);
+        lead.setPhoneNumber(0);
+        lead.setEmail(null);
+        lead.setCompanyName(null);
     }
     
 
--- src/test/java/Classes/CRMTest.java
+++ src/test/java/Classes/CRMTest.java
@@ -1,9 +1,66 @@
 package Classes;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CRMTest {
+    private Lead lead1;
+    private Lead empty_lead;
+    private CRM test_crm;
+    private Contact contact1;
 
+
+    @BeforeEach
+    public void setUp() {
+        test_crm = new CRM();
+        lead1 = new Lead("John Smith", 123456789, "jsmith@example.com", "Company A");
+        test_crm.leadList.put(lead1.getId(), lead1);
+        //empty_lead = new Lead("", 0, "", "");
+        empty_lead = new Lead();
+    }
+
+    @Test
+    void lookupLead_goodData_Works() {
+        assertEquals(lead1, test_crm.lookupLead(lead1.getId()));
+    }
+    @Test
+    void lookupLead_invalidID_IllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> test_crm.lookupLead("999"));
+        assertThrows(IllegalArgumentException.class, () -> test_crm.lookupLead("-78"));
+        assertThrows(IllegalArgumentException.class, () -> test_crm.lookupLead("word"));
+    }
+
+    @Test
+    void createContact_goodData_Works() {
+        String lead1ID = lead1.getId();
+        contact1 = test_crm.createContact(lead1);
+        //info matches
+        assertEquals("John Smith", contact1.getName());
+        assertEquals(123456789, contact1.getPhoneNumber());
+        assertEquals("jsmith@example.com", contact1.getEmail());
+        //lead deleted
+        assertThrows(IllegalArgumentException.class, () -> test_crm.lookupLead(lead1ID));
+    }
+
+    @Test
+    void createContact_emptyLead_IllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class , () -> test_crm.createContact(empty_lead));
+    }
+
+    @Test
+    void deleteLead_goodData_Works() {
+        test_crm.deleteLead(lead1);
+        assertEquals(null, lead1.getId());
+        assertEquals(null, lead1.getName());
+        assertEquals(0, lead1.getPhoneNumber());
+        assertEquals(null, lead1.getEmail());
+    }
+    @Test
+    void deleteLead_invalidLead_Throws() {
+        assertThrows(IllegalArgumentException.class, () -> test_crm.deleteLead(empty_lead));
+    }
 
 
 }
