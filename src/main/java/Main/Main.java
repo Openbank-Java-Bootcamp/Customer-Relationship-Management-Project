b6b9ef1d44fcba41a97bbb33dc003b7f2502fdb4
Elizabeth Sames <sames@uwalumni.com>
2022-05-04 01:05:54 +0200

    Added menu in CRM.

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,22 +2,17 @@ package Main;
 
 
 import Classes.CRM;
-import Classes.Lead;
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-
-
-
-
-
-
-
+        CRM crm = new CRM();
+        crm.menu(scanner);
 
         scanner.close();
     }
+
 }
